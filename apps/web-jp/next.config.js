23a139b4c9ab326dec2cfb937404eaa083d36258
DI-badch <dong.chi.hoang.ba@dataimpact.vn>
2024-12-27 09:22:47 +0700

    update deploy

--- apps/web-jp/next.config.js
+++ apps/web-jp/next.config.js
@@ -1,4 +1,4 @@
-// next.config.cjs
+// next.config.js
 
 const nextConfig = {
   reactStrictMode: true,
@@ -14,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
